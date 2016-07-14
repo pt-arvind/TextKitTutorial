c60af83bf0f509ed44edd75689aaf0712046ec77
Arvind Subramanian <arvind@weddingwire.com>
2016-07-14 15:32:47 -0400

    Initial commit

---
+++ WebViews/ForumsWebView/ForumsWebView/ForumCell.swift
@@ -0,0 +1,9 @@
+//
+//  ForumCell.swift
+//  ForumsWebView
+//
+//  Created by Arvind Subramanian on 7/14/16.
+//  Copyright © 2016 WeddingWire. All rights reserved.
+//
+
+import Foundation
---
+++ WebViews/ForumsWebView/ForumsWebView/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ForumsWebView
+//
+//  Created by Arvind Subramanian on 7/14/16.
+//  Copyright © 2016 WeddingWire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //view.backgroundColor = .greenColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
